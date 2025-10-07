983a7f087ac6ba340a99738ffdafeadd46d4b83c
shane-chapman-23 <chapmanshane23@gmail.com>
2025-10-07 14:56:15 +1300

    Added profile pic, mobile first styling

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -5,7 +5,13 @@ export default function Landing() {
   return (
     <>
       <div className={styles.landingContainer}>
-        <section className={styles.image}></section>
+        <section className={styles.image}>
+          <img
+            src="/images/shane.jpg"
+            alt="picture of Shane"
+            className={styles.image}
+          ></img>
+        </section>
         <section className={styles.introduction}>
           <h2>Shane Chapman</h2>
           <div className={styles.titleContainer}>
@@ -16,7 +22,8 @@ export default function Landing() {
             </h1>
           </div>
           <p className={styles.technologies}>
-            Javascript, React, Node.js, Express.
+            Javascript, React, Node.js, Express, <br />
+            and MORE!
           </p>
           <ContactButtons />
         </section>

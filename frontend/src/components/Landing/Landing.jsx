4c4e9fcd7a719d169e82cd7bfbec85b72f28a526
shane-chapman-23 <chapmanshane23@gmail.com>
2025-10-10 21:17:55 +1300

    Added react-icons. Styling to the landing

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,3 +1,11 @@
+import {
+  SiReact,
+  SiNodedotjs,
+  SiExpress,
+  SiJavascript,
+  SiMongodb,
+} from "react-icons/si";
+
 import ContactButtons from "./ContactButtons";
 import styles from "./Landing.module.css";
 
@@ -5,7 +13,7 @@ export default function Landing() {
   return (
     <>
       <div className={styles.landingContainer}>
-        <section className={styles.image}>
+        <section className={styles.imageSection}>
           <img
             src="/images/shane.jpg"
             alt="picture of Shane"
@@ -13,19 +21,26 @@ export default function Landing() {
           ></img>
         </section>
         <section className={styles.introduction}>
-          <h2>Shane Chapman</h2>
+          <ContactButtons />
           <div className={styles.titleContainer}>
-            <h1>
-              FULL-STACK
-              <br />
-              DEVELOPER
+            <h1 className={styles.title}>
+              Hi, I'm <span style={{ fontStyle: "italic" }}>Shane.</span> <br />
+              I like making <br />
+              Websites.
             </h1>
           </div>
           <p className={styles.technologies}>
-            Javascript, React, Node.js, Express, <br />
-            and MORE!
+            <SiJavascript title="JavaScript" />
+            Javascript
+            <SiReact title="React" />
+            React
+            <SiNodedotjs title="Node.js" />
+            Node.js
+            <SiExpress title="Express" />
+            Express
+            <SiMongodb title="MongoDB" />
+            MongoDB
           </p>
-          <ContactButtons />
         </section>
       </div>
     </>
